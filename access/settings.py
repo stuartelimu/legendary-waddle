2642c68eae9cff33e64575f9c541e2062ff1e1fa
stuartelimu <stuartelimu@gmail.com>
2021-01-25 19:52:59 +0300

    comment staticfiles path

--- access/settings.py
+++ access/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
